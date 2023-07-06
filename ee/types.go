e18a4040420ce669679c60baac7b43b5de78661f
TKaxv_7S <954067342@qq.com>
2023-07-06 17:37:50 +0800

    clear code

--- ee/types.go
+++ ee/types.go
@@ -1,41 +1,5 @@
 package ee
 
-import "fmt"
-
-const (
-	api_endpoint               = "https://api.accounts.netmaker.io/api/v1/license/validate"
-	license_cache_key          = "license_response_cache"
-	license_validation_err_msg = "invalid license"
-	server_id_key              = "nm-server-id"
-)
-
-var errValidation = fmt.Errorf(license_validation_err_msg)
-
-// LicenseKey - the license key struct representation with associated data
-type LicenseKey struct {
-	LicenseValue  string `json:"license_value"` // actual (public) key and the unique value for the key
-	Expiration    int64  `json:"expiration"`
-	LimitServers  int    `json:"limit_servers"`
-	LimitUsers    int    `json:"limit_users"`
-	LimitHosts    int    `json:"limit_hosts"`
-	LimitNetworks int    `json:"limit_networks"`
-	LimitClients  int    `json:"limit_clients"`
-	Metadata      string `json:"metadata"`
-	IsActive      bool   `json:"is_active"` // yes if active
-}
-
-// ValidatedLicense - the validated license struct
-type ValidatedLicense struct {
-	LicenseValue     string `json:"license_value" binding:"required"`     // license that validation is being requested for
-	EncryptedLicense string `json:"encrypted_license" binding:"required"` // to be decrypted by Netmaker using Netmaker server's private key
-}
-
-// LicenseSecret - the encrypted struct for sending user-id
-type LicenseSecret struct {
-	AssociatedID string        `json:"associated_id" binding:"required"` // UUID for user foreign key to User table
-	Limits       LicenseLimits `json:"limits" binding:"required"`
-}
-
 // LicenseLimits - struct license limits
 type LicenseLimits struct {
 	Servers  int `json:"servers"`
@@ -52,15 +16,4 @@ func (l *LicenseLimits) SetDefaults() {
 	l.Hosts = 0
 	l.Users = 1
 	l.Networks = 0
-}
-
-// ValidateLicenseRequest - used for request to validate license endpoint
-type ValidateLicenseRequest struct {
-	LicenseKey     string `json:"license_key" binding:"required"`
-	NmServerPubKey string `json:"nm_server_pub_key" binding:"required"` // Netmaker server public key used to send data back to Netmaker for the Netmaker server to decrypt (eg output from validating license)
-	EncryptedPart  string `json:"secret" binding:"required"`
-}
-
-type licenseResponseCache struct {
-	Body []byte `json:"body" binding:"required"`
 }
--- logic/telemetry.go
+++ logic/telemetry.go
@@ -2,24 +2,16 @@ package logic
 
 import (
 	"encoding/json"
-	"time"
-
 	"github.com/gravitl/netmaker/database"
-	"github.com/gravitl/netmaker/logger"
 	"github.com/gravitl/netmaker/models"
-	"github.com/gravitl/netmaker/servercfg"
-	"github.com/posthog/posthog-go"
+	"time"
 )
 
 // flags to keep for telemetry
 var isFreeTier bool
 var isEE bool
 
-// posthog_pub_key - Key for sending data to PostHog
-const posthog_pub_key = "phc_1vEXhPOA1P7HP5jP2dVU9xDTUqXHAelmtravyZ1vvES"
-
-// posthog_endpoint - Endpoint of PostHog server
-const posthog_endpoint = "https://app.posthog.com"
+var lastSendTime int64 = 0
 
 // setEEForTelemetry - store EE flag without having an import cycle when used for telemetry
 // (as the ee package needs the logic package as currently written).
@@ -33,115 +25,13 @@ func SetFreeTierForTelemetry(freeTierFlag bool) {
 	isFreeTier = freeTierFlag
 }
 
-// sendTelemetry - gathers telemetry data and sends to posthog
 func sendTelemetry() error {
-	if servercfg.Telemetry() == "off" {
-		return nil
-	}
-
-	var telRecord, err = fetchTelemetryRecord()
-	if err != nil {
-		return err
-	}
-	// get telemetry data
-	d, err := fetchTelemetryData()
-	if err != nil {
-		return err
-	}
-	client, err := posthog.NewWithConfig(posthog_pub_key, posthog.Config{Endpoint: posthog_endpoint})
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
-	// send to posthog
-	return client.Enqueue(posthog.Capture{
-		DistinctId: telRecord.UUID,
-		Event:      "daily checkin",
-		Properties: posthog.NewProperties().
-			Set("nodes", d.Nodes).
-			Set("hosts", d.Hosts).
-			Set("servers", d.Servers).
-			Set("non-server nodes", d.Count.NonServer).
-			Set("extclients", d.ExtClients).
-			Set("users", d.Users).
-			Set("networks", d.Networks).
-			Set("linux", d.Count.Linux).
-			Set("darwin", d.Count.MacOS).
-			Set("windows", d.Count.Windows).
-			Set("freebsd", d.Count.FreeBSD).
-			Set("docker", d.Count.Docker).
-			Set("k8s", d.Count.K8S).
-			Set("version", d.Version).
-			Set("is_ee", isEE).
-			Set("is_free_tier", isFreeTier),
-	})
+	return nil
 }
 
-// fetchTelemetry - fetches telemetry data: count of various object types in DB
-func fetchTelemetryData() (telemetryData, error) {
-	var data telemetryData
-
-	data.ExtClients = getDBLength(database.EXT_CLIENT_TABLE_NAME)
-	data.Users = getDBLength(database.USERS_TABLE_NAME)
-	data.Networks = getDBLength(database.NETWORKS_TABLE_NAME)
-	data.Hosts = getDBLength(database.HOSTS_TABLE_NAME)
-	data.Version = servercfg.GetVersion()
-	data.Servers = getServerCount()
-	nodes, err := GetAllNodes()
-	if err == nil {
-		data.Nodes = len(nodes)
-		data.Count = getClientCount(nodes)
-	}
-	return data, err
-}
-
-// getServerCount returns number of servers from database
-func getServerCount() int {
-	data, err := database.FetchRecords(database.SERVER_UUID_TABLE_NAME)
-	if err != nil {
-		logger.Log(0, "errror retrieving server data", err.Error())
-	}
-	return len(data)
-}
-
-// setTelemetryTimestamp - Give the entry in the DB a new timestamp
 func setTelemetryTimestamp(telRecord *models.Telemetry) error {
-	lastsend := time.Now().Unix()
-	var serverTelData = models.Telemetry{
-		UUID:           telRecord.UUID,
-		LastSend:       lastsend,
-		TrafficKeyPriv: telRecord.TrafficKeyPriv,
-		TrafficKeyPub:  telRecord.TrafficKeyPub,
-	}
-	jsonObj, err := json.Marshal(&serverTelData)
-	if err != nil {
-		return err
-	}
-	err = database.Insert(database.SERVER_UUID_RECORD_KEY, string(jsonObj), database.SERVER_UUID_TABLE_NAME)
-	return err
-}
-
-// getClientCount - returns counts of nodes with various OS types and conditions
-func getClientCount(nodes []models.Node) clientCount {
-	var count clientCount
-	for _, node := range nodes {
-		host, err := GetHost(node.HostID.String())
-		if err != nil {
-			continue
-		}
-		switch host.OS {
-		case "darwin":
-			count.MacOS += 1
-		case "windows":
-			count.Windows += 1
-		case "linux":
-			count.Linux += 1
-		case "freebsd":
-			count.FreeBSD += 1
-		}
-	}
-	return count
+	lastSendTime = time.Now().Unix()
+	return nil
 }
 
 // fetchTelemetryRecord - get the existing UUID and Timestamp from the DB
@@ -154,39 +44,6 @@ func fetchTelemetryRecord() (models.Telemetry, error) {
 		return telObj, err
 	}
 	err = json.Unmarshal([]byte(rawData), &telObj)
+	telObj.LastSend = lastSendTime
 	return telObj, err
-}
-
-// getDBLength - get length of DB to get count of objects
-func getDBLength(dbname string) int {
-	data, err := database.FetchRecords(dbname)
-	if err != nil {
-		return 0
-	}
-	return len(data)
-}
-
-// telemetryData - What data to send to posthog
-type telemetryData struct {
-	Nodes      int
-	Hosts      int
-	ExtClients int
-	Users      int
-	Count      clientCount
-	Networks   int
-	Servers    int
-	Version    string
-	IsEE       bool
-	IsFreeTier bool
-}
-
-// clientCount - What types of netclients we're tallying
-type clientCount struct {
-	MacOS     int
-	Windows   int
-	Linux     int
-	FreeBSD   int
-	K8S       int
-	Docker    int
-	NonServer int
 }
